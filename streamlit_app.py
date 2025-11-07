269691931515ca7c6905ac1a36ff20a693f2e6d7
josteb715 <jodisteberl@gmail.com>
2025-11-07 11:19:35 -0600

    Update streamlit_app.py
    new line after last line

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,7 @@ smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/wa
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=TRUE)
 
+
     
 
 
